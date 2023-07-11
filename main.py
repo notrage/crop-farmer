1f98dc6b309f720750d578f3f2bab4b52e2864aa
NotRage <grousson.dylan@gmail.com>
2023-07-11 13:36:49 +0200

    finished

--- main.py
+++ main.py
@@ -30,13 +30,13 @@ def pause(direction):
     for i in range(1000000):
         if keyboard.is_pressed('end'): stop()
         if keyboard.is_pressed('p'): break
+        if keyboard.is_pressed('home'): main()
         time.sleep(1)
         
     keyboard.press('k')
     time.sleep(2)
     keyboard.press(direction)
-    print("CropFarmer unpaused")
-    
+    print("CropFarmer unpaused") 
 
 def wait(n, direction):
     
@@ -44,6 +44,8 @@ def wait(n, direction):
         if keyboard.is_pressed('end'): stop()
         if keyboard.is_pressed('p'): pause(direction)
         time.sleep(1)
+       
+       
         
 def replace_93():
     
@@ -54,11 +56,17 @@ def replace_93():
     time.sleep(0.5)
     keyboard.release('z')
     
-def replace_155():
+def replace_melon_pump():
     
     keyboard.press('z')
     time.sleep(1.5)
     keyboard.release('z')
+    
+def replace_cocoa():
+    
+    keyboard.press('q')
+    time.sleep(1.5)
+    keyboard.release('q')  
     
 def left_strafe_93():
     
@@ -106,18 +114,39 @@ def right_strafe_155():
     wait(73, 'd')
     keyboard.release('d')
     
+def front_diagonal_327():
+    
+    #print("debug: front diagonal 327")
+    keyboard.press('q')
+    wait(50, 'q')
+    keyboard.release('q')
+    
+def back_diagonal_327():
+    
+    #print("debug: back diagonal 327")
+    keyboard.press('s')
+    wait(50, 's')
+    keyboard.release('s')
+    
 def front_strafe_155():
     
-    print("todo")
+    #print("debug: front 155")
+    keyboard.press('z')
+    wait(72, 'z')
+    keyboard.release('z')
     
 def back_strafe_155():
     
-    print("todo")
+    #print("debug: back 155")
+    keyboard.press('s')
+    wait(72, 's')
+    keyboard.release('s')
     
-
+    
 
 def wheat_or_potato():
     
+    #print("debug: wheat or potato cycle")
     for i in range(2):
         left_strafe_93()
         right_strafe_93()
@@ -126,48 +155,51 @@ def wheat_or_potato():
     
 def carrot_or_wart():
     
+    #print("debug: carrot or wart cycle")
     for i in range(2):
         right_strafe_93()
         left_strafe_93()
 
     right_strafe_93()
     
-def cocoa_bean():
-    
-    print("todo")
-    
-def sugar_cane():
-    
-    print("todo")
-    
-def cactus():
-    
-    print("todo")
-    
 def melon_or_pumpkin():
     
+    #print("debug: melon or pumpkin")
     for i in range(4):
         
         right_strafe_155()
-        replace_155()
+        replace_melon_pump()
         left_strafe_155()
-        replace_155()
+        replace_melon_pump()
         
     right_strafe_155()
     
-def mushroom():
+def sugar_cane():
     
-    print("todo")
+    #print("debug: sugar cane cycle")
+    for i in range(4):
+        front_diagonal_327()
+        back_diagonal_327()
+        
+    front_diagonal_327()
 
-
-
-if __name__ == '__main__':
+def cocoa_bean():
     
-    print("CropFarmer Starting\n")
+    #print("debug: cocoa bean cycle")
+    for i in range(8):
+        
+        front_strafe_155()
+        replace_cocoa()
+        back_strafe_155()
+        replace_cocoa()
+
+def main(): 
     
     to_farm = input("For farm wheat or potato : 1\n"
-                    "For farm wart or carrot  : 2\n"
-                    "For farm melon or pumpkin: 3\n")
+                    "For farm carrot or wart  : 2\n"
+                    "For farm melon or pumpkin: 3\n"
+                    "For farm sugar cane      : 4\n"
+                    "For farm cocoa beans     : 5\n")
     
     wait(10, '')
 
@@ -181,3 +213,17 @@ if __name__ == '__main__':
             case '1': wheat_or_potato()
             case '2': carrot_or_wart()
             case '3': melon_or_pumpkin()
+            case '4': sugar_cane()
+            case '5': cocoa_bean()
+            case _  : to_farm = input("For farm wheat or potato : 1\n"
+                                      "For farm carrot or wart  : 2\n"
+                                      "For farm melon or pumpkin: 3\n"
+                                      "For farm sugar cane      : 4\n"
+                                      "For farm cocoa beans     : 5\n")
+            
+if __name__ == '__main__':
+    
+    print("CropFarmer Starting\n")
+    main()
+    
+    
